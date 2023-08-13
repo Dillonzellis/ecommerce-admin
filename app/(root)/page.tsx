d68797630e0807f1f93d5269deed2493ccf8f52f
dillonzellis <dillonzellis@gmail.com>
2023-08-13 12:21:58 -0400

    add base store creation modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,25 @@
-import { Button } from "@/components/ui/button";
+'use client'
+
+import { useEffect } from "react";
+
 import { UserButton } from "@clerk/nextjs";
+import { useStoreModal } from "@/hooks/use-modal-store";
+
 
 export default function Home() {
+
+  const onOpen = useStoreModal((state) => state.onOpen)
+  const isOpen = useStoreModal((state) => state.isOpen)
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onOpen])
+
   return (
     <div className='min-h-screen bg-black text-slate-50'>
-      <div>
-        <Button>Yeppers</Button>
+      <div className="p-8">
         <UserButton afterSignOutUrl="/" />
       </div>
     </div>
